b018d63cfc35d8399c4c5640f03dd7abc2dacf51
hannah-merseal <hmm5708@psu.edu>
2021-03-29 10:36:53 -0400

    new tempo class variable

--- tempoanalysis.R
+++ tempoanalysis.R
@@ -1,9 +1,18 @@
-###Tempo stuff
+###Tempo Stuff
 source("analysis.R")
 source("easy_util.R")
 library(hexbin)
 
-#add notes/sec and quartiles; filter wjd_inphrase to JUST lick & line, split tempi
+##list of important plots
+#ngram_tempoclass_plot(): new tempo classes, facet is n-gram
+#tempoclass_plot(): new tempo classes, facet is tempo class
+#ngram_tempo_quart_plot(): tempo quartiles, facet is n-gram
+#quartile_tempo_plot(): tempo quartiles, facet is quartile
+#ngram_tempo_trad_plot(): classical tempi, facet is n-gram (do not use)
+#trad_tempo_plot(): classical tempi, facet is class (do not use)
+
+
+#add notes/sec and quartiles; filter wjd_inphrase to JUST lick & line, split tempi, make new bins for tempoclass
 wjd_inphrase_ll <- wjd_inphrase %>%
   dplyr::mutate(notespersec = (N)/(dur)) %>%
   filter(MLA_main_type == c("lick", "line")) %>%
@@ -13,9 +22,16 @@ wjd_inphrase_ll <- wjd_inphrase %>%
                                      120, 160, 200, 500),
                           labels = c("largo", "adagio", "andante", 
                                      "moderato", "allegro", "presto", "prestissimo"))) %>%
-  mutate(notespersec_quartile = ntile(notespersec, 4))
+  mutate(notespersec_quartile = ntile(notespersec, 4)) %>%
+  mutate(tempoclass = recode(tempoclass,
+                             "SLOW" = "SLOW",
+                             "MEDIUM SLOW" = "MEDIUM",
+                             "MEDIUM" = "MEDIUM",
+                             "MEDIUM UP" = "UP",
+                             "UP" = "UP")) %>%
+  mutate(tempoclass = factor(tempoclass, levels = c("SLOW", "MEDIUM", "UP")))
 
-#exploring the tempo class variable - not using it
+#the tempo class variable is now split into 3 bins instead of 5
 genre_tempo_boxplot <- ggplot(wjd_inphrase_ll, aes(x = factor(style), y = avgtempo)) + 
   geom_boxplot()
 
@@ -31,6 +47,148 @@ genre_tempo_hist <- ggplot(wjd_inphrase_ll, aes(x = avgtempo)) +
 class_tempo_hist <- ggplot(wjd_inphrase_ll, aes(x = avgtempo)) +
   geom_histogram(fill = "white", color = "black") +
   facet_grid(tempoclass ~ .)
+
+#new tempo class plot where facet is n-gram
+ngram_tempoclass_plot <- function(data = wjd_inphrase_ll, 
+                                   min_N = 3, 
+                                   min_freq = 1, 
+                                   max_pos = 30, 
+                                   fix_scale = FALSE,
+                                   standardize = FALSE,
+                                   facetting = TRUE,
+                                   easiness = "combined_easiness",
+                                   easiness_label = "Mean Difficulty",
+                                   smooth = "poly", poly_degree = 3){
+  #data$easiness <- data %>% pull(!!rlang::enquo(easiness))
+  data$easiness <- as.data.frame(data)[, easiness]
+  if(standardize){
+    data <- data %>% group_by(N) %>% mutate(easiness = scale(easiness)) %>% ungroup()
+  }  
+  #browser()
+  data <- 
+    data %>% 
+    filter(N >= min_N, in_phrase, freq >= min_freq, phrase_pos <= max_pos, phrase_len >= N) %>%  
+    group_by(N, phrase_pos, tempoclass) %>% 
+    summarise(easiness_mean = mean(easiness), 
+              easiness_sd = sd(easiness), 
+              n = n(), 
+              easiness_se = easiness_sd/sqrt(n())) %>% 
+    ungroup() 
+  mean_eff_size <- 
+    data %>% 
+    group_by(N) %>% 
+    summarise(r = diff(range(easiness_mean)), mem = mean(easiness_mean)) %>% 
+    print() %>% 
+    summarise(d = mean(r), max_d = max(r), min_d = min(r), med_d = median(r), mem = mean(mem)) %>% 
+    print() %>% 
+    pull(med_d)
+  #browser()
+  cor <- my_cor_test(data %>% filter(N == 4), "phrase_pos", "easiness_mean") 
+  mean_cor <- 
+    map_dfr(3:10, function(x) {
+      my_cor_test(data %>% filter(N == x), "phrase_pos", "easiness_mean")}) %>% 
+    summarise(mean_cor = mean(estimate)) %>% 
+    print()
+  
+  printf("Data points: %d, mean eff. size: %f", sum(data$n), mean_eff_size)
+  
+  q <- 
+    data %>% 
+    ggplot(aes(x = phrase_pos, y = easiness_mean, group = factor(tempoclass), color = factor(tempoclass))) 
+  q <- q + geom_errorbar(aes(ymin = easiness_mean - easiness_se, ymax = easiness_mean + easiness_se)) 
+  if(facetting){
+    if(fix_scale){
+      q <- q + facet_wrap(~N) 
+    }
+    else{
+      q <- q + facet_wrap(~N, scales = "free_y") 
+    }
+    q <- q + theme(legend.position = "none") 
+  }
+  if(smooth == "poly"){
+    f <- sprintf("y ~ poly(x, %d)", poly_degree)
+    q <- q + geom_smooth(method = "lm", formula = f) 
+  }
+  else{
+    q <- q + geom_smooth() 
+    
+  }
+  q <- q + theme_bw() 
+  q <- q + labs(x = "Phrase position", y  = easiness_label, caption  = sprintf("N = %d", sum(data$n))) 
+  q <- q + geom_point()
+  q
+}
+
+#tempo class plot where facet is new tempo class, n-gram = 3
+tempoclass_plot <- function(data = wjd_inphrase_ll, 
+                                min_N = 3, 
+                                min_freq = 1, 
+                                max_pos = 30, 
+                                fix_scale = FALSE,
+                                standardize = FALSE,
+                                facetting = TRUE,
+                                easiness = "combined_easiness",
+                                easiness_label = "Mean Difficulty",
+                                smooth = "poly", poly_degree = 3){
+  #data$easiness <- data %>% pull(!!rlang::enquo(easiness))
+  data$easiness <- as.data.frame(data)[, easiness]
+  if(standardize){
+    data <- data %>% group_by(N) %>% mutate(easiness = scale(easiness)) %>% ungroup()
+  }  
+  #browser()
+  data <- 
+    data %>% 
+    filter(N == min_N, in_phrase, freq >= min_freq, phrase_pos <= max_pos, phrase_len >= N) %>%  
+    group_by(N, phrase_pos, tempoclass) %>% 
+    summarise(easiness_mean = mean(easiness), 
+              easiness_sd = sd(easiness), 
+              n = n(), 
+              easiness_se = easiness_sd/sqrt(n())) %>% 
+    ungroup() 
+  mean_eff_size <- 
+    data %>% 
+    group_by(N) %>% 
+    summarise(r = diff(range(easiness_mean)), mem = mean(easiness_mean)) %>% 
+    print() %>% 
+    summarise(d = mean(r), max_d = max(r), min_d = min(r), med_d = median(r), mem = mean(mem)) %>% 
+    print() %>% 
+    pull(med_d)
+  #browser()
+  cor <- my_cor_test(data %>% filter(N == 4), "phrase_pos", "easiness_mean") 
+  mean_cor <- 
+    map_dfr(3:10, function(x) {
+      my_cor_test(data %>% filter(N == x), "phrase_pos", "easiness_mean")}) %>% 
+    summarise(mean_cor = mean(estimate)) %>% 
+    print()
+  
+  printf("Data points: %d, mean eff. size: %f", sum(data$n), mean_eff_size)
+  
+  q <- 
+    data %>% 
+    ggplot(aes(x = phrase_pos, y = easiness_mean, color = factor(tempoclass))) 
+  q <- q + geom_errorbar(aes(ymin = easiness_mean - easiness_se, ymax = easiness_mean + easiness_se)) 
+  if(facetting){
+    if(fix_scale){
+      q <- q + facet_wrap(~tempoclass) 
+    }
+    else{
+      q <- q + facet_wrap(~tempoclass, scales = "free_y") 
+    }
+    q <- q + theme(legend.position = "none") 
+  }
+  if(smooth == "poly"){
+    f <- sprintf("y ~ poly(x, %d)", poly_degree)
+    q <- q + geom_smooth(method = "lm", formula = f) 
+  }
+  else{
+    q <- q + geom_smooth() 
+    
+  }
+  q <- q + theme_bw() 
+  q <- q + labs(x = "Phrase position", y  = easiness_label, caption  = sprintf("N = %d", sum(data$n))) 
+  q <- q + geom_point()
+  q
+}
 
 #tempo quartile plot where facet is n-gram
 ngram_tempo_quart_plot <- function(data = wjd_inphrase_ll, 
